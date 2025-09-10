d4237b4eab5853da0a99642033c4eb443bd97bd3
nick whalen <nicktwhalen@gmail.com>
2025-09-09 21:52:12 -0400

    added ngrok-skip-browser-warning header to requests

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -41,6 +41,7 @@ class ApiClient {
     const config: RequestInit = {
       headers: {
         "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "true",
         ...options.headers,
       },
       ...options,
